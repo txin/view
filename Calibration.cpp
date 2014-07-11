2c1ae7f335504b9abe9b18d072b199517b79e9b2
Tianxin Tang <titian.tang@gmail.com>
2014-07-11 10:58:49 -0500

    added calibration part of code, needs debugging

--- Calibration.cpp
+++ Calibration.cpp
@@ -25,7 +25,25 @@ class Settings {
 public:
     Settings(): goodInput(false) {} 
     
-    void read(const FileNode& node) {                          //Read serialization for this class
+    void write(FileStorage& fs) const {//Write serialization for this class
+        fs << "{" << "BoardSize_Width"  << boardSize.width
+           << "BoardSize_Height" << boardSize.height
+           << "Square_Size"         << squareSize
+           << "Calibrate_NrOfFrameToUse" << nrFrames
+           << "Calibrate_FixAspectRatio" << aspectRatio
+           << "Calibrate_AssumeZeroTangentialDistortion" << calibZeroTangentDist
+           << "Calibrate_FixPrincipalPointAtTheCenter" << calibFixPrincipalPoint
+           << "Write_DetectedFeaturePoints" << bwritePoints
+           << "Write_extrinsicParameters"   << bwriteExtrinsics
+           << "Write_outputFileName"  << outputFileName
+           << "Show_UndistortedImage" << showUndistorsed
+           << "Input_FlipAroundHorizontalAxis" << flipVertical
+           << "Input_Delay" << delay
+           << "Input" << input
+           << "}";
+    }
+
+    void read(const FileNode& node) {       //Read serialization for this class
         node["BoardSize_Width" ] >> boardSize.width;
         node["BoardSize_Height"] >> boardSize.height;
         node["Square_Size"]  >> squareSize;
@@ -42,10 +60,12 @@ public:
         node["Input_Delay"] >> delay;
         interprate();
     }
+
     void interprate() {
         goodInput = true;
         if (boardSize.width <= 0 || boardSize.height <= 0) {
-            cerr << "Invalid Board size: " << boardSize.width << " " << boardSize.height << endl;
+            cerr << "Invalid Board size: " << boardSize.width << " " 
+                 << boardSize.height << endl;
             goodInput = false;
         }
         if (squareSize <= 10e-6) {
@@ -56,7 +76,6 @@ public:
             cerr << "Invalid number of frames " << nrFrames << endl;
             goodInput = false;
         }
-
         // default input images files
         if (input.empty())      // Check for valid input
             goodInput = false;
@@ -66,7 +85,8 @@ public:
             }
             else {
                 if (readStringList(input, imageList)) {
-                    nrFrames = (nrFrames < (int)imageList.size()) ? nrFrames : (int)imageList.size();
+                    nrFrames = (nrFrames < (int)imageList.size()) ? 
+                        nrFrames : (int)imageList.size();
                 }
             }
         }
@@ -75,6 +95,14 @@ public:
         if(calibZeroTangentDist)   flag |= CV_CALIB_ZERO_TANGENT_DIST;
         if(aspectRatio)            flag |= CV_CALIB_FIX_ASPECT_RATIO;
         atImageList = 0;
+    }
+
+    Mat nextImage() {
+        Mat result;
+        if (atImageList < (int)imageList.size() ) {
+            result = imread(imageList[atImageList++], CV_LOAD_IMAGE_COLOR);
+        }
+        return result;
     }
 
     static bool readStringList(const string& filename, vector<string>& l) {
@@ -93,16 +121,16 @@ public:
 
 // default pattern: CHESSBOARD
 public:
-    Size boardSize;            // The size of the board -> Number of items by width and height
-    float squareSize;          // The size of a square in your defined unit (point, millimeter,etc).
-    int nrFrames;              // The number of frames to use from the input for calibration
-    float aspectRatio;         // The aspect ratio
+    Size boardSize;           
+    float squareSize;         
+    int nrFrames;             
+    float aspectRatio;        
     int delay;                 // In case of a video input
     bool bwritePoints;         //  Write detected feature points
     bool bwriteExtrinsics;     // Write extrinsic parameters
     bool calibZeroTangentDist; // Assume zero tangential distortion
     bool calibFixPrincipalPoint;// Fix the principal point at the center
-    bool flipVertical;          // Flip the captured images around the horizontal axis
+    bool flipVertical;         // Flip the captured images around the horizontal axis
     string outputFileName;      // The name of the file where to write
     bool showUndistorsed;       // Show undistorted images after calibration
     string input;              
@@ -115,12 +143,156 @@ private:
     string patternToUse;
 };
 
-static void read(const FileNode& node, Settings& x, const Settings& default_value = Settings())
-{
+static void read(const FileNode& node, Settings& x, 
+                 const Settings& default_value = Settings()) {
     if(node.empty())
         x = default_value;
     else
         x.read(node);
+}
+
+static double computeReprojectionErrors(const vector<vector<Point3f> >& objectPoints,
+                                        const vector<vector<Point2f> >& imagePoints,
+                                        const vector<Mat>& rvecs, 
+                                        const vector<Mat>& tvecs, 
+                                        const Mat& cameraMatrix , 
+                                        const Mat& distCoeffs,
+                                        vector<float>& perViewErrors) {
+    vector<Point2f> imagePoints2;
+    int i, totalPoints = 0;
+    double totalErr = 0, err;
+    perViewErrors.resize(objectPoints.size());
+
+    for( i = 0; i < (int)objectPoints.size(); ++i) {
+        projectPoints( Mat(objectPoints[i]), rvecs[i], tvecs[i], cameraMatrix,
+                       distCoeffs, imagePoints2);
+        err = norm(Mat(imagePoints[i]), Mat(imagePoints2), CV_L2);
+        int n = (int)objectPoints[i].size();
+        perViewErrors[i] = (float) std::sqrt(err*err/n);
+        totalErr        += err*err;
+        totalPoints     += n;
+    }
+    return std::sqrt(totalErr/totalPoints);
+}
+
+static void calcBoardCornerPositions(Size boardSize, float squareSize, 
+                                     vector<Point3f>& corners) {
+    corners.clear();
+    for( int i = 0; i < boardSize.height; ++i )
+        for( int j = 0; j < boardSize.width; ++j )
+            corners.push_back(Point3f(float(j*squareSize), float(i*squareSize), 0));
+}
+
+static bool runCalibration(Settings& s, Size& imageSize, Mat& cameraMatrix,
+                           Mat& distCoeffs, vector<vector<Point2f> > imagePoints, 
+                           vector<Mat>& rvecs, vector<Mat>& tvecs, 
+                           vector<float>& reprojErrs,  double& totalAvgErr) {
+    cameraMatrix = Mat::eye(3, 3, CV_64F);
+    if( s.flag & CV_CALIB_FIX_ASPECT_RATIO )
+        cameraMatrix.at<double>(0,0) = 1.0;
+    distCoeffs = Mat::zeros(8, 1, CV_64F);
+    vector<vector<Point3f> > objectPoints(1);
+    calcBoardCornerPositions(s.boardSize, s.squareSize, objectPoints[0]);
+    objectPoints.resize(imagePoints.size(),objectPoints[0]);
+
+    //Find intrinsic and extrinsic camera parameters
+    double rms = calibrateCamera(objectPoints, imagePoints, imageSize, cameraMatrix,
+                                 distCoeffs, rvecs, tvecs, 
+                                 s.flag|CV_CALIB_FIX_K4|CV_CALIB_FIX_K5);
+
+    cout << "Re-projection error reported by calibrateCamera: "<< rms << endl;
+    bool ok = checkRange(cameraMatrix) && checkRange(distCoeffs);
+    totalAvgErr = computeReprojectionErrors(objectPoints, imagePoints,
+                                            rvecs, tvecs, cameraMatrix,
+                                            distCoeffs, reprojErrs);
+    return ok;
+}
+
+// Print camera parameters to the output file
+static void saveCameraParams( Settings& s, Size& imageSize,
+                              Mat& cameraMatrix, Mat& distCoeffs,
+                              const vector<Mat>& rvecs, const vector<Mat>& tvecs,
+                              const vector<float>& reprojErrs, 
+                              const vector<vector<Point2f> >& imagePoints,
+                              double totalAvgErr ) {
+    FileStorage fs( s.outputFileName, FileStorage::WRITE );
+    time_t tm;
+    time( &tm );
+    struct tm *t2 = localtime( &tm );
+    char buf[1024];
+    strftime( buf, sizeof(buf)-1, "%c", t2 );
+    fs << "calibration_Time" << buf;
+    if( !rvecs.empty() || !reprojErrs.empty() )
+        fs << "nrOfFrames" << (int)std::max(rvecs.size(), reprojErrs.size());
+    fs << "image_Width" << imageSize.width;
+    fs << "image_Height" << imageSize.height;
+    fs << "board_Width" << s.boardSize.width;
+    fs << "board_Height" << s.boardSize.height;
+    fs << "square_Size" << s.squareSize;
+
+    if(s.flag & CV_CALIB_FIX_ASPECT_RATIO )
+        fs << "FixAspectRatio" << s.aspectRatio;
+
+    if(s.flag) {
+        sprintf( buf, "flags: %s%s%s%s",
+                 s.flag & CV_CALIB_USE_INTRINSIC_GUESS ? " +use_intrinsic_guess" : "",
+                 s.flag & CV_CALIB_FIX_ASPECT_RATIO ? " +fix_aspectRatio" : "",
+                 s.flag & CV_CALIB_FIX_PRINCIPAL_POINT ? " +fix_principal_point" : "",
+                 s.flag & CV_CALIB_ZERO_TANGENT_DIST ? " +zero_tangent_dist" : "" );
+        cvWriteComment( *fs, buf, 0 );
+
+    }
+    fs << "flagValue" << s.flag;
+    fs << "Camera_Matrix" << cameraMatrix;
+    fs << "Distortion_Coefficients" << distCoeffs;
+    fs << "Avg_Reprojection_Error" << totalAvgErr;
+    if( !reprojErrs.empty() )
+        fs << "Per_View_Reprojection_Errors" << Mat(reprojErrs);
+    if( !rvecs.empty() && !tvecs.empty() )    {
+        CV_Assert(rvecs[0].type() == tvecs[0].type());
+        Mat bigmat((int)rvecs.size(), 6, rvecs[0].type());
+        for( int i = 0; i < (int)rvecs.size(); i++ )
+        {
+            Mat r = bigmat(Range(i, i+1), Range(0,3));
+            Mat t = bigmat(Range(i, i+1), Range(3,6));
+
+            CV_Assert(rvecs[i].rows == 3 && rvecs[i].cols == 1);
+            CV_Assert(tvecs[i].rows == 3 && tvecs[i].cols == 1);
+            //*.t() is MatExpr (not Mat) so we can use assignment operator
+            r = rvecs[i].t();
+            t = tvecs[i].t();
+        }
+        cvWriteComment( *fs, "a set of 6-tuples (rotation vector + translation vector) for each view", 0 );
+        fs << "Extrinsic_Parameters" << bigmat;
+    }
+
+    if( !imagePoints.empty() )
+    {
+        Mat imagePtMat((int)imagePoints.size(), (int)imagePoints[0].size(), CV_32FC2);
+        for( int i = 0; i < (int)imagePoints.size(); i++ )
+        {
+            Mat r = imagePtMat.row(i).reshape(2, imagePtMat.cols);
+            Mat imgpti(imagePoints[i]);
+            imgpti.copyTo(r);
+        }
+        fs << "Image_points" << imagePtMat;
+    }
+}
+
+bool runCalibrationAndSave(Settings& s, Size imageSize, Mat&  cameraMatrix, 
+                           Mat& distCoeffs,vector<vector<Point2f> > imagePoints ) {
+    vector<Mat> rvecs, tvecs;
+    vector<float> reprojErrs;
+    double totalAvgErr = 0;
+
+    bool ok = runCalibration(s,imageSize, cameraMatrix, distCoeffs, 
+                             imagePoints, rvecs, tvecs,reprojErrs, totalAvgErr);
+    cout << (ok ? "Calibration succeeded" : "Calibration failed")
+         << ". avg re projection error = "  << totalAvgErr ;
+    if( ok )
+        saveCameraParams( s, imageSize, cameraMatrix, distCoeffs, rvecs ,
+                          tvecs, reprojErrs, imagePoints, totalAvgErr);
+    return ok;
 }
 
 int main(int argc, char** argv) {
@@ -128,7 +300,8 @@ int main(int argc, char** argv) {
     const string inputSettingsFile = "default.xml";
     FileStorage fs(inputSettingsFile, FileStorage::READ);
     if (!fs.isOpened()) {
-        cout << "Could not open the configuration file: \"" << inputSettingsFile << "\"" << endl;
+        cout << "Could not open the configuration file: \"" << inputSettingsFile 
+             << "\"" << endl;
     }
     fs["Settings"] >> s;
     fs.release();
@@ -136,6 +309,71 @@ int main(int argc, char** argv) {
     if (!s.goodInput) {
         cout << "Invalid input detected. Application stopping." << endl;
         return -1;
+    }
+
+    vector< vector<Point2f> > imagePoints;
+    Mat cameraMatrix, distCoeffs;
+    Size imageSize;
+    int mode = 0; // IMAGE_LIST
+    const char ESC_KEY = 27;
+
+    for (int i = 0; ; ++i) {
+        Mat view;
+        
+        view = s.nextImage();
+// If no more images then run calibration, save and stop loop.
+        if (view.empty()) {
+            if(imagePoints.size() > 0) {
+                runCalibrationAndSave(s, imageSize,
+                                      cameraMatrix, distCoeffs, imagePoints);
+            }
+            break;
+        }
+        imageSize = view.size();
+        if (s.flipVertical) {
+            flip(view, view, 0);
+        }    
+        vector<Point2f> pointBuf;
+        bool found;
+        found = findChessboardCorners(view, s.boardSize, pointBuf,
+                                      CV_CALIB_CB_ADAPTIVE_THRESH 
+                                      | CV_CALIB_CB_FAST_CHECK 
+                                      | CV_CALIB_CB_NORMALIZE_IMAGE);
+        // pattern: chessboard
+        if (found) {
+            Mat viewGray;
+            cvtColor(view, viewGray, COLOR_BGR2GRAY);
+            cornerSubPix(viewGray, pointBuf, Size(11,11),
+                         Size(-1,-1), 
+                         TermCriteria( CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 30, 0.1));
+            drawChessboardCorners(view, s.boardSize, Mat(pointBuf), found);
+        }
+        imshow("Image View", view);
+        char key = (char)waitKey(s.delay);
+        if (key == ESC_KEY) {
+            break;
+        }
+    }
+
+    // -----------------------Show the undistorted image for the image list ------
+    // default image list
+    if (s.showUndistorsed) {
+        Mat view, rview, map1, map2;
+        initUndistortRectifyMap(cameraMatrix, distCoeffs, Mat(),
+                                getOptimalNewCameraMatrix(cameraMatrix, 
+                                distCoeffs, imageSize, 1, imageSize, 0),
+                                imageSize, CV_16SC2, map1, map2);
+
+        for (int i = 0; i < (int)s.imageList.size(); i++) {
+            view = imread(s.imageList[i], 1);
+            if(view.empty())
+                continue;
+            remap(view, rview, map1, map2, INTER_LINEAR);
+            imshow("Image View", rview);
+            char c = (char)waitKey();
+            if( c  == ESC_KEY || c == 'q' || c == 'Q' )
+                break;
+        }
     }
     return 0;
 }
