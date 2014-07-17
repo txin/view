f78d1cc657d58ad86dbb7c08de99ef35ee248cf9
Tianxin Tang <titian.tang@gmail.com>
2014-07-17 11:24:54 +0100

    added cube with glm

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-CFLAGS = `pkg-config --cflags opencv` -std=c++11
+LAGS = `pkg-config --cflags opencv` -std=c++11
 LIBS = `pkg-config --libs opencv` -lpthread
 
 % : %.cpp
@@ -7,5 +7,8 @@ cube : cube.cpp
 	g++ -std=c++11 -c cube.cpp
 	g++ cube.o -o cube.out -lGL -lGLU -lglfw3 -lX11 -lXxf86vm -lXrandr -lpthread -lXi
 
+t : t.cpp shader.cpp shader.hpp
+	g++ -std=c++11 -c t.cpp shader.cpp shader.hpp
+	g++ t.o shader.o -o t.out -lGL -lGLU -lglfw3 -lX11 -lXxf86vm -lXrandr -lpthread -lXi -lGLEW
 clean :
 	rm *.o
--- cube.cpp
+++ cube.cpp
@@ -1,64 +1,109 @@
 /*
- * cube
- * openGL
- *
- *
+ * cube with openGL, glfw3
  */
 
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-static void error_callback(int error, const char* description)
-{
+static void error_callback(int error, const char* description) {
     fputs(description, stderr);
 }
 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
-{
+static void key_callback(GLFWwindow* window, int key, int scancode, 
+                         int action, int mods) {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
 }
 
-int main(void)
-{
+int main(void) {
+
     GLFWwindow* window;
+
     glfwSetErrorCallback(error_callback);
-    if (!glfwInit())
+    
+    if (!glfwInit()) {
         exit(EXIT_FAILURE);
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
-    if (!window)
-    {
+    }
+    
+    window = glfwCreateWindow(640, 480, "Cube", NULL, NULL);
+ 
+    if (!window) {
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
+    
     glfwMakeContextCurrent(window);
     glfwSetKeyCallback(window, key_callback);
-    while (!glfwWindowShouldClose(window))
-    {
+
+    while (!glfwWindowShouldClose(window)) {
         float ratio;
         int width, height;
+
         glfwGetFramebufferSize(window, &width, &height);
         ratio = width / (float) height;
         glViewport(0, 0, width, height);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
+
         glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+       
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
-        glRotatef((float) glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
-        glBegin(GL_TRIANGLES);
-        glColor3f(1.f, 0.f, 0.f);
-        glVertex3f(-0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 1.f, 0.f);
-        glVertex3f(0.6f, -0.4f, 0.f);
-        glColor3f(0.f, 0.f, 1.f);
-        glVertex3f(0.f, 0.6f, 0.f);
+        glRotatef((float) glfwGetTime() * 20.f, 0.f, 1.f, 0.f);
+        
+        // White side - BACK
+        glBegin(GL_POLYGON);
+        glColor3f(   1.0,  1.0, 1.0 );
+        glVertex3f(  0.5, -0.5, 0.5 );
+        glVertex3f(  0.5,  0.5, 0.5 );
+        glVertex3f( -0.5,  0.5, 0.5 );
+        glVertex3f( -0.5, -0.5, 0.5 );
         glEnd();
+ 
+// Purple side - RIGHT
+        glBegin(GL_POLYGON);
+        glColor3f(  1.0,  0.0,  1.0 );
+        glVertex3f( 0.5, -0.5, -0.5 );
+        glVertex3f( 0.5,  0.5, -0.5 );
+        glVertex3f( 0.5,  0.5,  0.5 );
+        glVertex3f( 0.5, -0.5,  0.5 );
+        glEnd();
+ 
+// Green side - LEFT
+        glBegin(GL_POLYGON);
+        glColor3f(   0.0,  1.0,  0.0 );
+        glVertex3f( -0.5, -0.5,  0.5 );
+        glVertex3f( -0.5,  0.5,  0.5 );
+        glVertex3f( -0.5,  0.5, -0.5 );
+        glVertex3f( -0.5, -0.5, -0.5 );
+        glEnd();
+ 
+// Blue side - TOP
+        glBegin(GL_POLYGON);
+        glColor3f(   0.0,  0.0,  1.0 );
+        glVertex3f(  0.5,  0.5,  0.5 );
+        glVertex3f(  0.5,  0.5, -0.5 );
+        glVertex3f( -0.5,  0.5, -0.5 );
+        glVertex3f( -0.5,  0.5,  0.5 );
+        glEnd();
+ 
+// Red side - BOTTOM
+        glBegin(GL_POLYGON);
+        glColor3f(   1.0,  0.0,  0.0 );
+        glVertex3f(  0.5, -0.5, -0.5 );
+        glVertex3f(  0.5, -0.5,  0.5 );
+        glVertex3f( -0.5, -0.5,  0.5 );
+        glVertex3f( -0.5, -0.5, -0.5 );
+        glEnd();
+ 
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
     glfwDestroyWindow(window);
     glfwTerminate();
     exit(EXIT_SUCCESS);
