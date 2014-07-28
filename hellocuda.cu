1493a409f9fa128c415f1d160c10874ec150546e
Tianxin Tang <titian.tang@gmail.com>
2014-07-28 17:37:43 +0100

    tested CUDA vectors

--- hellocuda.cu
+++ hellocuda.cu
@@ -1,35 +1,51 @@
-//#include <cuda.h>
-//#include <cuda_runtime.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define N 512
 
 __global__ void add(int *a, int *b, int *c) {
-    *c = *a + *b;
+    c[blockIdx.x] = a[blockIdx.x] + b[blockIdx.x];
 }
 
+
+void random_ints(int* a, int N) {
+   int i;
+   for (i = 0; i < N; ++i)
+       a[i] = rand();
+}
+
+
 int main(void) {
-    int a, b, c;
+    int *a, *b, *c;
     int *d_a, *d_b, *d_c;
-    int size = sizeof(int);
+    int size = N * sizeof(int);
 
     cudaMalloc((void **)&d_a, size);
     cudaMalloc((void **)&d_b, size);
     cudaMalloc((void **)&d_c, size);
     
-    a = 2;
-    b = 7;
+    a = (int *)malloc(size);
+    random_ints(a, N);
+    b = (int *)malloc(size);
+    random_ints(b, N);
+    c = (int *)malloc(size);
     
-    cudaMemcpy(d_a, &a, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, &b, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
 
-    add<<<1, 1>>>(d_a, d_b, d_c);
+    add<<<N, 1>>>(d_a, d_b, d_c);
 
-    cudaMemcpy(&c, d_c, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
     
+    /*for (int i = 0; i < N; i++) {
+        printf("%d", c[i]);
+        }*/
+    
+    free(a); free(b); free(c);
+
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_c);
-
-    printf("c=%d\n", c);
     
     return 0;
 }
