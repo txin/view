095b0eae06cad3ab67beadf24b723a8a0c8601b4
Tianxin Tang <titian.tang@gmail.com>
2014-07-30 11:44:12 +0100

    basic StereoBM_GPU working

--- gpuStereo.cpp
+++ gpuStereo.cpp
@@ -6,32 +6,45 @@
 
 // apply the stereoBM_GPU method
 int main(int argc, char* argv[]) {
-    cv::gpu::StereoBM_GPU stereo;
+    cv::gpu::StereoBM_GPU sbm;
 
     try {
         cv::Mat srcLeftImg = cv::imread("left.ppm", CV_LOAD_IMAGE_GRAYSCALE);
         cv::Mat srcRightImg = cv::imread("right.ppm", CV_LOAD_IMAGE_GRAYSCALE);
 
-        cv::gpu::GpuMat dst, src;
+        cv::gpu::GpuMat dstL, srcL;
         cv::gpu::GpuMat dstR, srcR;
-        src.upload(srcLeftImg);
+        srcL.upload(srcLeftImg);
         srcR.upload(srcRightImg);
+        cv::gpu::GpuMat disp, disp8;
+        //int nDisparities = 16 * 10;
+        //int SADWindowSize = 51;
+        //cv::gpu::StereoBM_GPU sbm(cv::gpu::StereoBM_GPU::BASIC_PRESET,
+        //                          nDisparities, SADWindowSize);
+        cv::gpu::StereoBM_GPU sbm;
+        sbm(srcL, srcR, disp);
 
-        cv::gpu::threshold(src, dst, 128.0, 255.0, CV_THRESH_BINARY);
+        cv::gpu::normalize(disp, disp8, 0, 255, CV_MINMAX, CV_8U);
+        
+
+        /* cv::gpu::threshold(srcL, dstL, 128.0, 255.0, CV_THRESH_BINARY);
         cv::gpu::threshold(srcR, dstR, 128.0, 255.0, CV_THRESH_BINARY);
 
-        cv::Mat result_host = (cv::Mat)dst;
-        cv::Mat resultHostRight = (cv::Mat)dstR;
+        cv::Mat result_host = (cv::Mat)dstL;
+        cv::Mat resultHostRight = (cv::Mat)dstR; */
         
-        std::string windowNames[] = {"Left", "Right"};
-        
-        for (int i = 0; i < 2; i++) {
-            cv::namedWindow(windowNames[i], CV_WINDOW_AUTOSIZE);
-            cv::moveWindow(windowNames[i], 500 * i, 0);
-        }
 
-        cv::imshow("Left", result_host);
-        cv::imshow("Right", resultHostRight);
+        std::string windowNames[] = {"Left", "Right", "Disparity"};
+        
+        for (int i = 0; i < 3; i++) {
+            cv::namedWindow(windowNames[i], CV_WINDOW_AUTOSIZE);
+            cv::moveWindow(windowNames[i], 480 * i, 0);
+        }
+        cv::Mat resultDisp8 = (cv::Mat)disp8;
+        cv::imshow(windowNames[2], resultDisp8);
+
+        cv::imshow("Left", srcLeftImg);
+        cv::imshow("Right", srcRightImg);
         cv::waitKey();
     } catch(const cv::Exception& ex) {
         std::cout << "Error: " << ex.what() << std::endl;
